a2082dcf1a7bcc55a513d433203eec56b7c3d3b6
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-04-15 21:19:02 -0400

    Modified communityPage with appropriate divs for styling.

--- client/src/pages/CommunityPage/CommunityPage.js
+++ client/src/pages/CommunityPage/CommunityPage.js
@@ -1,20 +1,26 @@
 import React from "react";
-import Navbar from "../../components/Navbar/Navbar";
+import Navbar from "../../components/CommunityNavbar/CommunityNavbar";
 import CommunityJumbo from "../../components/CommunityJumbotron/CommunityJumbo";
 import NewsJumbo from "../../components/NewsJumbo/NewsJumbo";
-// import Capitol from "../../assets/images/Capitol"
-import CommunityCards from "../../components/CommunityCards/CommunityCards"
+import CommunityCards from "../../components/CommunityCards/CommunityCards";
+import "./style.css"
 
 function CommunityPage() {
   return (
-    <div>
-      <Navbar />
-      <div class="container jumbotronContainer">
-        <CommunityJumbo />
-        <NewsJumbo />
+  <div>
+     <Navbar />
+      <div className="container jumboCards">
+
+        <div className="container jumbotronContainer">
+          <CommunityJumbo />
+          <NewsJumbo />
+        </div>
+
+        <CommunityCards/>
+
       </div>
-      <CommunityCards/>
-    </div>
+  </div>
+    
   );
 }
 export default CommunityPage;

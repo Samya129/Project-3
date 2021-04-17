e304517131c7060daaf444ed579a14f039282b6d
turtles <me@ramon.nyc>
2021-04-17 08:00:55 -0700

    cleared header

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import Header from "./components/Header"
 import Footer from "./components/Footer"
-
 import Login from "./components/Login"
 import Signup from "./components/Signup";
 
 function App() {
   return (
     <div>
-      <Header/>
       <Login />
       <Signup/>
       <Footer/>

266f736560a8db146683bc6bc104fe8fa4274d25
turtles <me@ramon.nyc>
2021-04-18 18:04:13 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,19 @@
+const mongoose = require("mongodb");
+mongoose.Promise = global.Promise;
+
+const db = {};
+
+db.mongoose = mongoose;
+
+db.user = require("./user.model");
+db.role = require("./role.model");
+
+db.ROLES = ["user", "admin"];
+
+module.exports = db;
+
+/* OLD CODE
 module.exports = {
   Book: require("./book")
 };
+*/

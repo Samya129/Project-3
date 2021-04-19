e1ead9400fc72e0e8939a1aa36872c7adc83c8b9
turtles <me@ramon.nyc>
2021-04-18 18:01:20 -0700

    switching user.js
    commented out user.js so I could use the user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,35 +1,18 @@
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-  username: {
-    type: String,
-    trim: true,
-    required: "Username is Required"
-  },
-
-  password: {
-    type: String,
-    trim: true,
-    required: "Password is Required",
-    validate: [({ length }) => length >= 6, "Password should be longer."]
-  },
-
-  email: {
-    type: String,
-    unique: true,
-    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
-  },
-
-  userCreated: {
-    type: Date,
-    default: Date.now
-  }
-
-});
-
-
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role"
+      }
+    ]
+  })
+);
 
 module.exports = User;

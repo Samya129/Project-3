eb86c3403a90e8cd6bfc97e01ef0ae4637401b86
turtles <me@ramon.nyc>
2021-04-22 18:13:27 -0700

    Login functionality
    Adding portions of code from exisiting template into current code CSS strcture.

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,44 +1,96 @@
 import React, { Component } from "react";
 import "./style.css";
 import Pic from "../../assets/images/login.png";
+import { Redirect } from 'react-router-dom';
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
 
+import { connect } from "react-redux";
+import { login } from "../../actions/auth";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
 
 class Login extends Component {
   // Setting the component's initial state
-  state = {
-    email: "",
-    password: ""
-  };
+  constructor(props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
 
-  handleInputChange = event => {
-    // Getting the value and name of the input which triggered the change
-    // Next to handle special characters at the moment
-    const { name, value } = event.target;
+    this.state = {
+      username: "",
+      password: "",
+      loading: false,
+    };
+  }
 
-    // Updating the input's state
+  onChangeUsername(e) {
     this.setState({
-      [name]: value
+      username: e.target.value,
     });
-  };
+  }
 
-  handleFormSubmit = event => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    event.preventDefault();
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
 
-  };
+  handleLogin(e) {
+    e.preventDefault();
+
+    this.setState({
+      loading: true,
+    });
+
+    this.form.validateAll();
+
+    const { dispatch, history } = this.props;
+
+    if (this.checkBtn.context._errors.length === 0) {
+      dispatch(login(this.state.username, this.state.password))
+        .then(() => {
+          history.push("/YourCommunity");
+          window.location.reload();
+        })
+        .catch(() => {
+          this.setState({
+            loading: false
+          });
+        });
+    } else {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
 
   render() {
       // Notice how each input has a `value`, `name`, and `onChange` prop
   return (
     <div className= "container">
+
       <div>
         <img src={Pic} alt="loginImage" className="img-fluid"/>
       </div>
+
       <div className= "form col-md-6 offset-md-3">
         <div className="title">Log in
-        </div>
-        <div className= "container2">
-          <form>
+      </div>
+
+      <div className= "container2">
+
+            <form>
             <input
                 value={this.state.email}
                 name="email"
@@ -55,6 +107,7 @@ class Login extends Component {
                 placeholder="Enter Password:"
               />
           </form>
+
         </div>
         <br></br>
         <button type="submit" className="btn btn-light btn-block" onClick={this.handleFormSubmit}>Log in</button>
